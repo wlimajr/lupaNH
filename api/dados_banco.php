37c9f365a8d53c116c4f520375b9d98a5ee4028e
Altemir Almeida <altemir.git@gmail.com>
2017-06-13 00:05:05 -0300

    teste

--- api/dados_banco.php
+++
@@ -1,8 +0,0 @@
-<?php
-    //host, nome do banco, usuario, senha
-    $dados_banco = array(
-        'host'=>'mysql:host=localhost;dbname=hiperlocal',
-        'usuario'=>'root', 
-        'senha'=>'teste321');
-
-?>
